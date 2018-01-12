f01f8b296f348c3c9ea6a5e02de40548f61d547b
rahulPatil <rp7187655@gmail.com>
2018-01-12 14:25:57 +0530

    redirecting to index page when accessing page is not defined.

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 const timeStamp = require('./time.js').timeStamp;
 const webApp = require('./webapp');
-const handlers = require('./handlers.js');
 const fs = require('fs');
+
+let getContentType=function(file){
+  let fileDetails = file.split('.');
+  let extension = fileDetails[1];
+  let mimeType={
+    "html" : "text/html",
+    "css" : "text/css",
+    "jpg" : "img/jpg",
+    "gif" : "img/gif",
+    "js" : "text/javascript",
+    "pdf" : "application/pdf"
+  }
+  return mimeType[extension];
+}
 
 let app=webApp.create();
 
 app.get('/',(req,res)=>{
   res.redirect('/index.html');
 });
+
+app.get('/index.html',(req,res)=>{
+  let content=fs.readFileSync("./public/index.html");
+  res.setHeader('Content-Type',getContentType('/index.html'));
+  res.write(content);
+  res.end();
+})
 
 module.exports = app;
--- test/appTest.js
+++ test/appTest.js
@@ -13,4 +13,26 @@ describe('app',()=>{
       })
     })
   })
+
+  describe('GET /',()=>{
+    it('redirects to index.html',done=>{
+      request(app,{method:'GET',url:'/'},(res)=>{
+        th.should_be_redirected_to(res,'/index.html');
+        assert.equal(res.body,"");
+        done();
+      })
+    })
+  })
+
+  describe('GET /index.html',()=>{
+    it('gives the index page',done=>{
+      request(app,{method:'GET',url:'/index.html'},res=>{
+        th.status_is_ok(res);
+        th.content_type_is(res,'text/html');
+        th.body_contains(res,"userName");
+        th.body_contains(res,"password");
+        done();
+      })
+    })
+  })
 })
